27af773925e7947a62a8773ca8610539c0af30d2
alpsjur <alpsjur@runbox.com>
2026-07-22 14:24:32 +0000

    added missing value

--- sweep_gls_c4.sh
+++ sweep_gls_c4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the range of GLS_C4 values
-GLS_C4_VALUES=(0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5)
+GLS_C4_VALUES=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5)
 
 # Base input file
 BASE_INPUT_FILE="testmix.in"
